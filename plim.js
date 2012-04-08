34e5201b271ce662ec02872ce6e673f1a8c662fe
busticated <itsbusticated@gmail.com>
2012-04-08 15:00:37 -0700

    1st cut at finding & loading a custom config file

--- plim.js
+++ plim.js
@@ -3,23 +3,65 @@
 // TODO - enable on the fly rjs optimization via connect middleware
 // TODO - some kind of unit test runner (plim -t <runs all known unit tests>)
 // TODO - add some kind of documentation generator (JSDoc? LessDoC?)
+// TODO - add a 'setup' routine where standard dirs & files (css, js, index.html) are created based on user input
 
 (function(){
     "use strict";
 
     var fs = require( 'fs' ),
+        path = require('path'),
         deps = [ 'commander', 'less', 'requirejs', 'express' ],
         prog = {},
         options = {
             port: 3000,
+            baseUrl: process.cwd(),
             jsBuildCfg: 'js/build.js',
             lessSrc: 'css/',
             lessOut: 'css/',
             lessMain: 'css/main.less'
         },
-        log, install, init, start, optimize, npm, rjs, express, less, app;
+        npm, rjs, express, less, app;
 
-    log = function( msg, showName ){
+    var exists = function() {
+        var obj = fs.existsSync ? fs : path;
+        return obj.existsSync.apply( obj, arguments );
+    };
+
+    var findFile = function( name, dir ) {
+        dir = dir || process.cwd();
+
+        var filePath = path.normalize( path.join( dir, name ) );
+
+        if ( exists( filePath ) ) {
+            return filePath;
+        }
+
+        return dir === "/" ?
+            null : findFile( name, path.normalize( path.join( dir, ".." ) ) );
+    };
+
+    var findConfig = function() {
+        var name = "plim.config",
+            projConfig = findFile( name ),
+            globalConfig = path.normalize( path.join( process.env.HOME, name ) );
+
+        if ( projConfig ) {
+            return projConfig;
+        }
+
+        if ( exists( globalConfig ) ) {
+            return globalConfig;
+        }
+
+        return false;
+    };
+
+    var loadConfig = function ( filePath ) {
+        return filePath && exists( filePath ) ?
+                JSON.parse( fs.readFileSync( filePath, "utf-8" ) ) : options;
+    };
+
+    var log = function( msg, showName ){
         msg = msg || '';
         showName = typeof showName === 'undefined' ? true : showName;
 
@@ -36,16 +78,24 @@
         console.log( msg );
     };
 
-    // command-line interface
-    init = function(){
+    // enable command-line interface & setup configs
+    var init = function(){
         prog = require( 'commander' );
         prog
-            .version( '0.0.2' )
+            .version( '0.0.5' )
             .option( '-s, --setup', 'Setup and install dependencies' )
             .option( '-p, --port <port>', 'Set server port (default is 3000)', parseInt, 3000 )
             .option( '-P, --production', 'Run in production mode (uses optimized resource files)' )
-            .option( '-o, --optimize [build file]', 'Run rjs AMD js optimization (default build file is "js/build.js")' )
+            .option( '-o, --optimizeJS [build file]', 'Run rjs AMD js optimization (default build file is "js/build.js")' )
             .option( '-l, --less [value]', 'Set "main" less file to target when forcing less recompilation' );
+
+        options = loadConfig( findConfig() );
+
+        console.log( options );
+        console.log( path.join( options.baseUrl, options.lessMain ) );
+        console.log(  path.join( options.baseUrl, options.lessSrc ) );
+
+        //process.exit();
 
         prog.on( '--help', function(){
             log();
@@ -57,8 +107,8 @@
             options.lessMain = prog.less;
         }
 
-        if ( prog.optimize ){
-            optimize( typeof prog.optimize === 'boolean' ? 'js/build.js' : prog.optimize );
+        if ( prog.optimizeJS ){
+            optimizeJS( typeof prog.optimizeJS === 'boolean' ? 'js/build.js' : prog.optimizeJS );
             process.exit();
         }
 
@@ -70,7 +120,7 @@
     };
 
     // config & start server
-    start = function( port ){
+    var start = function( port ){
         var lessCfg = {
             compress: false,
             force: true //meh... still no worky probably need this to land: https://github.com/cloudhead/less.js/pull/503
@@ -101,16 +151,16 @@
                 });
             } else {
                 app.use( function( req, res, next ){
-                    fs.utimesSync( options.lessMain, new Date(), new Date() );
+                    fs.utimesSync( path.join( options.baseUrl, options.lessMain ), new Date(), new Date() );
                     next();
                 });
             }
             app.use( express.compiler({
-                    src: __dirname + '/',
+                    src: path.normalize( options.baseUrl ),
                     enable: [ 'less' ]
                 })
             );
-            app.use( express.static( __dirname + '/', { maxAge: 0 } ) );
+            app.use( express.static( path.normalize( options.baseUrl ), { maxAge: 0 } ) );
             app.use( express.errorHandler({
                     dumpExceptions: true,
                     showStack: true
@@ -122,15 +172,15 @@
         app.listen( port );
     };
 
-    optimize = function( cfgFilePath ){
+    var optimizeJS = function( cfgFilePath ){
         log( '::::::::: optimizing AMD JS files' );
 
-        var config =  eval( fs.readFileSync( cfgFilePath, 'utf8' ) ); // oh my!
+        var buildCfg =  eval( fs.readFileSync( cfgFilePath, 'utf8' ) ); // oh my!
 
         rjs = require( 'requirejs' );
 
         try {
-            rjs.optimize( config, function( data ){
+            rjs.optimize( buildCfg, function( data ){
                 log( data, false );
             });
         } catch( err ){
@@ -138,7 +188,7 @@
         }
     };
 
-    install = function( deps ){
+    var install = function( deps ){
         var depsToInstall = [];
 
         npm = require( 'npm' );
